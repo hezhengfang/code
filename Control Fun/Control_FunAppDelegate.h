a7a7ce066df736210cec28a85f4cb368e69bf520
hezhengfang <hezhengfang>
2012-11-23 12:52:21 +0800

    day05 control fun

---
+++ Control Fun/Control_FunAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  Control_FunAppDelegate.h
+//  Control Fun
+//
+//  Created by geine on 11/23/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class Control_FunViewController;
+
+@interface Control_FunAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet Control_FunViewController *viewController;
+
+@end
